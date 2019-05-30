00526cb1214522ce0d476e62cad0c99a1f653150
John Emerson <jemerson20@dxc.com>
2019-05-30 12:56:31 +0100

    First Commit

---
+++ src/test/java/myapppackage/Stepdefs.java
@@ -0,0 +1,59 @@
+package myapppackage;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+    @Given("I have entered a valid Username")
+    public void i_have_entered_a_valid_Username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have entered a valid Password")
+    public void i_have_entered_a_valid_Password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I select SignIn")
+    public void i_select_SignIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the Admin Landing Page is displayed")
+    public void the_Admin_Landing_Page_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have entered an invalid Password")
+    public void i_have_entered_an_invalid_Password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("a login error message is displayed")
+    public void a_login_error_message_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I have entered a valid username of {string}")
+    public void i_have_entered_a_valid_username_of(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("^I have entered a valid password of \"([^\"]*)\"$")
+    public void i_have_entered_a_valid_password_of(String password) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
---
+++ src/test/resources/myapppackage/admin_login.feature
@@ -0,0 +1,35 @@
+Feature: AdminLogin
+  In order to blah
+  As a somebody
+  I want to do something
+
+
+
+  Scenario: Admin Login
+    Given I have entered a valid Username
+    And I have entered a valid Password
+    When I select SignIn
+    Then the Admin Landing Page is displayed
+
+  Scenario: Admin Login Invalid Password
+    Given I have entered a valid Username
+    And I have entered an invalid Password
+    When I select SignIn
+    Then a login error message is displayed
+
+  Scenario: Admin Login Again
+    Given I have entered a valid Username
+    And I have entered a valid Password
+    When I select SignIn
+    Then a login error message is displayed
+
+  Scenario Outline: Admin Login Outline
+    Given I have entered a valid username of "<Username>"
+    And I have entered a valid password of "<Password>"
+    When I select SignIn
+    Then the Admin Landing Page is displayed
+
+    Examples:
+      | Username | Password             |
+      | tomsmith | SuperSecretPassword! |
+      | archie   | SuperSecretPassword  |
