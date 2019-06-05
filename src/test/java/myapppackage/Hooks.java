7f35fa047ec1be90660ee52009b77a370a9eead1
John Emerson <jemerson20@dxc.com>
2019-06-05 08:16:02 +0100

    Tidy up Hooks prior to new branch

---
+++ src/test/java/myapppackage/Hooks.java
@@ -0,0 +1,26 @@
+package myapppackage;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import myapppackage.pageObjects.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+
+    WebDriver driver;
+
+    public Hooks(){
+
+    }
+
+    @Before
+    public void BeforeTestRun(){
+
+    }
+
+    @After
+    public void AfterTestRun(){
+
+    }
+}
