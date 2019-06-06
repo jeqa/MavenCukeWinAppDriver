c888700388da7335985316f865353f4f318314fc
John Emerson <jemerson20@dxc.com>
2019-06-06 14:22:06 +0100

    Created StepDefintions folder and moved Stepdefs to it

--- src/test/java/myapppackage/StepDefintions/Stepdefs.java
+++ src/test/java/myapppackage/StepDefintions/Stepdefs.java
@@ -1,8 +1,9 @@
-package myapppackage;
+package myapppackage.StepDefintions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import myapppackage.WebDriverController;
 import org.junit.Assert;
 import myapppackage.pageObjects.LoginPage;
 
