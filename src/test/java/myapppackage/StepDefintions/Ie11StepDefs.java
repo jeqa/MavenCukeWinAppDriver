ca8904e26ea91b29532eadd214a76a90b3e2c9ec
administrator <jemerson20@dxc.com>
2019-07-19 06:01:09 -0700

    First Test working

---
+++ src/test/java/myapppackage/StepDefintions/Ie11StepDefs.java
@@ -0,0 +1,48 @@
+package myapppackage.StepDefintions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import myapppackage.WebDriverController;
+import myapppackage.pageObjects.ChromeApp;
+import myapppackage.pageObjects.Ie11App;
+import org.junit.Assert;
+
+
+public class Ie11StepDefs {
+
+    //private WebDriver driver;
+    private WebDriverController webDriverController;
+    private Ie11App ie11App;
+
+    private String fileName;
+
+    public Ie11StepDefs(WebDriverController webDriverController){
+        this.webDriverController = webDriverController;
+        ie11App = new Ie11App(this.webDriverController);
+    }
+
+    @Given("that IE has opened")
+    public void that_IE_has_opened() {
+        //Handle possible Set Up dialog
+        //ie11App.handleIeSetUpDialog();
+    }
+
+    @Then("the expected home page is displayed in IE")
+    public void the_expected_home_page_is_displayed_in_IE() {
+        Assert.assertTrue("Actual Address is: " + ie11App.getCurrentUrl(),
+                ie11App.getCurrentUrl().equalsIgnoreCase(ie11App.getHomeUrl()));
+    }
+
+    @When("I attempt to navigate to another site in IE")
+    public void i_attempt_to_navigate_to_another_site_in_IE() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the expected site is displayed in IE")
+    public void the_expected_site_is_displayed_in_IE() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
