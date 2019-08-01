36e267dedfadedccbf452e44f1da4131745fa683
sahmed248 <sahmed248@dxc.com>
2019-08-01 12:57:53 +0100

    Update WebDriverController.java
    Removing commented out code

--- src/test/java/myapppackage/WebDriverController.java
+++ src/test/java/myapppackage/WebDriverController.java
@@ -29,7 +29,7 @@ public class WebDriverController {
 
             this.driver = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
             Assert.assertNotNull(driver);
-            //if (!appIdentifier.contains("chrome")){
+    
             if (toBeMaximised){
                 this.driver.manage().window().maximize();}
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
