daa8b99fa5d047bff01a042da4ac4e10a1e1350e
sahmed248 <sahmed248@dxc.com>
2019-08-12 10:21:54 +0100

    Update AcrobatReaderApp.java
    Remove extra space

--- src/test/java/myapppackage/pageObjects/AcrobatReaderApp.java
+++ src/test/java/myapppackage/pageObjects/AcrobatReaderApp.java
@@ -7,7 +7,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
