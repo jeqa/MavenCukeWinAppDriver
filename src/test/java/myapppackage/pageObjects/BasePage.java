9146d9cd53964604366858a69868182158782f04
sahmed248 <sahmed248@dxc.com>
2019-08-14 11:18:21 +0100

    Update BasePage.java
    Remove commented out code

--- src/test/java/myapppackage/pageObjects/BasePage.java
+++ src/test/java/myapppackage/pageObjects/BasePage.java
@@ -84,8 +84,7 @@ public class BasePage {
                 Find(element).click();
             }
         } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println(e.getMessage());
+           System.out.println(e.getMessage());
         }
     }
 
